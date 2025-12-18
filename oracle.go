63a2fd04557e6cb1659a9d70c11bc1ab5d439f01
liuoneice <liuoneice@163.com>
2025-12-18 14:16:59 +0800

    改为oceanbase oracle模式支持的初始query语句

--- oracle.go
+++ oracle.go
@@ -235,7 +235,7 @@ func (d Dialector) Initialize(db *gorm.DB) (err error) {
 			_ = go_ora.AddSessionParam(sqlDB, "NLS_SORT", "BINARY_CI")
 		}
 	}
-	err = db.ConnPool.QueryRowContext(context.Background(), "select version from product_component_version where rownum = 1").Scan(&d.DBVer)
+	err = db.ConnPool.QueryRowContext(context.Background(), "select OB_VERSION() from DUAL").Scan(&d.DBVer)
 	if err != nil {
 		return err
 	}
